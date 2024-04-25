d63b0278b92102dfe7ba735f23300bfbfe7d00d5
Ashish Khare <akstyles211@gmail.com>
2024-04-25 11:19:09 +0530

    feat: add roboto font

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "http://eternal-zen.vercel.app"
+  site: "http://eternal-zen.vercel.app",
 });
