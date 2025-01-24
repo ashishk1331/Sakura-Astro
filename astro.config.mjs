664e68a3665fa7e917a9e53ec47b36fe3f9969d3
Ashish Khare <akstyles211@gmail.com>
2025-01-24 17:15:40 +0530

    feat: reading time for blogs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
-import tailwind from '@astrojs/tailwind';
-import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
+import { remarkReadingTime } from "./src/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'http://eternal-zen.vercel.app',
-    integrations: [tailwind()],
+  site: "http://eternal-zen.vercel.app",
+  integrations: [tailwind()],
+  markdown: {
+    remarkPlugins: [remarkReadingTime],
+  },
 });
