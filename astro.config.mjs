ebc890439ee9f885f658032ac2689d9efeed8f0b
Ashish Khare <akstyles211@gmail.com>
2025-02-16 23:54:14 +0530

    fix: audio file corruption

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,10 @@ import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 import { remarkReadingTime } from "./src/remark-reading-time.mjs";
 
-import react from "@astrojs/react";
-
 // https://astro.build/config
 export default defineConfig({
   site: "http://eternal-zen.vercel.app",
-  integrations: [tailwind(), react()],
+  integrations: [tailwind()],
   markdown: {
     remarkPlugins: [remarkReadingTime],
   },
